29af4d974929ee798bb143406f5e21e7cd8ba008
torisaki <ngocbinhtran79@gmail.com>
2024-07-04 00:32:06 +0700

    product detail

--- src/BirdStoreAndroid/app/src/main/java/com/example/birdstoreandroid/Activity/LoginActivity.java
+++ src/BirdStoreAndroid/app/src/main/java/com/example/birdstoreandroid/Activity/LoginActivity.java
@@ -1,9 +1,7 @@
 package com.example.birdstoreandroid.Activity;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.credentials.CredentialManager;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
