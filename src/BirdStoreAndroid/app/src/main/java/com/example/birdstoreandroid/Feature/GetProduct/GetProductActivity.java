67f610fd6f5ecf97add1b5c61c20b72aa9e8384c
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2024-07-07 15:23:13 +0700

    update new layout for store main and product detail

--- src/BirdStoreAndroid/app/src/main/java/com/example/birdstoreandroid/Feature/GetProduct/GetProductActivity.java
+++ src/BirdStoreAndroid/app/src/main/java/com/example/birdstoreandroid/Feature/GetProduct/GetProductActivity.java
@@ -12,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.birdstoreandroid.API.ApiClient;
 import com.example.birdstoreandroid.Model.GetProductRequest;
@@ -24,29 +26,36 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class GetProductActivity extends AppCompatActivity {
+public class GetProductActivity extends AppCompatActivity implements GetProductAdapter.OnItemClickListener {
     private ListView listView;
     private GetProductAdapter productAdapter;
+
+    private RecyclerView product_recycler_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_get_product);
+//        setContentView(R.layout.activity_get_product);
+        setContentView(R.layout.store_main_layout);
 
-        listView = findViewById(R.id.lvGetProduct);
+//        listView = findViewById(R.id.lvGetProduct);
+
+        product_recycler_view = findViewById(R.id.product_recycler_view);
+        product_recycler_view.setLayoutManager(new LinearLayoutManager(this));
+
         fetchProducts();
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                GetProductRequest selectedProduct = (GetProductRequest) parent.getItemAtPosition(position);
-                String productId = selectedProduct.getId();
-
-                Intent intent = new Intent(GetProductActivity.this, ProductDetailActivity.class);
-                intent.putExtra("productId", productId);
-                startActivity(intent);
-            }
-        });
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                GetProductRequest selectedProduct = (GetProductRequest) parent.getItemAtPosition(position);
+//                String productId = selectedProduct.getId();
+//
+//                Intent intent = new Intent(GetProductActivity.this, ProductDetailActivity.class);
+//                intent.putExtra("productId", productId);
+//                startActivity(intent);
+//            }
+//        });
     }
 
     private void fetchProducts() {
@@ -55,8 +64,8 @@ public class GetProductActivity extends AppCompatActivity {
             public void onResponse(Call<GetProductResponse> call, Response<GetProductResponse> response) {
                 if (response.isSuccessful() && response.body() != null) {
                     List<GetProductRequest> products = response.body().getData();
-                    productAdapter = new GetProductAdapter(GetProductActivity.this, products);
-                    listView.setAdapter(productAdapter);
+                    productAdapter = new GetProductAdapter(GetProductActivity.this, products, GetProductActivity.this);
+                    product_recycler_view.setAdapter(productAdapter);
                 } else {
                     Toast.makeText(GetProductActivity.this, "Failed to fetch products", Toast.LENGTH_SHORT).show();
                 }
@@ -67,5 +76,14 @@ public class GetProductActivity extends AppCompatActivity {
                 Toast.makeText(GetProductActivity.this, "An error occurred: " + t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public void onItemClick(GetProductRequest product) {
+        String productId = product.getId();
+
+        Intent intent = new Intent(GetProductActivity.this, ProductDetailActivity.class);
+        intent.putExtra("productId", productId);
+        startActivity(intent);
     }
 }
