94ed2219efa28de8000077859b9804a1412766d6
Nguyễn Trọng Nghĩa <nguyentrongnghia2003@gmail.com>
2024-07-08 16:51:28 +0700

    update test zalopay

---
+++ src/BirdStoreAndroid/app/src/main/java/com/example/birdstoreandroid/Feature/ZaloPay/PaymentNotification.java
@@ -0,0 +1,27 @@
+package com.example.birdstoreandroid.Feature.ZaloPay;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.birdstoreandroid.R;
+
+public class PaymentNotification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_notification);
+
+        TextView txtNotification = findViewById(R.id.textViewNotify);
+
+        Intent intent = getIntent();
+        txtNotification.setText(intent.getStringExtra("result"));
+    }
+}
